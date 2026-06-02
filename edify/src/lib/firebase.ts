96404a1771020bd001fe3e57d161621719dcf18d
Eliel Kwesi Agbeke <brighteliel@gmail.com>
2026-06-02 14:28:57 +0000

    feat: initialize Firebase services and add .env files to gitignore

--- edify/src/lib/firebase.ts
+++ edify/src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getMessaging, isSupported } from "firebase/messaging"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBeOhX_qvMsf1SnGjSBVN29jpWQVbhX9uQ",
-  authDomain: "edify-bible-app.firebaseapp.com",
-  projectId: "edify-bible-app",
-  storageBucket: "edify-bible-app.firebasestorage.app",
-  messagingSenderId: "39135039352",
-  appId: "1:39135039352:web:2d822b42a8dad7d11e028d",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 }
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
