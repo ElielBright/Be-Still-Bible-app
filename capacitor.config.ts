51c2e7c06a3961286669746d4f8150790eb99d7c
Eliel Kwesi Agbeke <brighteliel@gmail.com>
2026-06-03 21:59:38 +0000

    feat: initialize Android platform and configure Capacitor project settings

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,20 +3,14 @@ import { CapacitorConfig } from "@capacitor/cli"
 const config: CapacitorConfig = {
   appId: "com.edify.bible",
   appName: "Edify",
-  webDir: ".next",
+  webDir: "public",
   server: {
-    androidScheme: "https",
+    url: "http://localhost:3000",
     cleartext: true,
   },
   plugins: {
     PushNotifications: {
       presentationOptions: ["badge", "sound", "alert"],
-    },
-  },
-  android: {
-    buildOptions: {
-      keystorePath: undefined,
-      keystoreAlias: undefined,
     },
   },
 }
